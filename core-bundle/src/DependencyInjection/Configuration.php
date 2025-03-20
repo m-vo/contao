2112eaec57e9e2e89024e9beb2231fb0c2efe52b
M. Vondano <m-vo@users.noreply.github.com>
2025-03-20 11:24:07 +0100

    WIP

--- core-bundle/src/DependencyInjection/Configuration.php
+++ core-bundle/src/DependencyInjection/Configuration.php
@@ -117,6 +117,7 @@ class Configuration implements ConfigurationInterface
                 ->append($this->addCspNode())
                 ->append($this->addAltchaNode())
                 ->append($this->addTemplateStudioNode())
+                ->append($this->addFileManagerNode())
             ->end()
         ;
 
@@ -990,6 +991,15 @@ class Configuration implements ConfigurationInterface
     private function addTemplateStudioNode(): NodeDefinition
     {
         return (new TreeBuilder('template_studio'))
+            ->getRootNode()
+            ->addDefaultsIfNotSet()
+            ->canBeDisabled()
+        ;
+    }
+
+    private function addFileManagerNode(): NodeDefinition
+    {
+        return (new TreeBuilder('file_manager'))
             ->getRootNode()
             ->addDefaultsIfNotSet()
             ->canBeDisabled()
